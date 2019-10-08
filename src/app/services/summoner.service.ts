75f0d1316e04fdec119fefe4f0dcca0e8c7dc82e
Simon G. García <hc0867@bbvaplataforma.cl>
2019-10-08 17:55:37 -0300

    Enter

--- src/app/services/summoner.service.ts
+++ src/app/services/summoner.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SummonerService {
+  api = 'RGAPI-83c61c42-921a-4fcf-b0bf-52b09eaafa50';
 
   constructor(private http: HttpClient) { }
 
-  getSummoner(){
-    return this.http.get('https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/eikichis?api_key=RGAPI-4c0d4ce8-119f-4909-9fcd-f12474491df9');
-  }
 
-  getLeague(){
-    return this.http.get('https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/p1rJYvyJpWCINqIvxSk0CulcYcyk977HXOToQBaWs9c?api_key=RGAPI-4c0d4ce8-119f-4909-9fcd-f12474491df9');
+  getSummoner() {
+    return this.http.get('http://localhost/lol/summoner/v4/summoners/by-name/eikichis?platform=LA2') 
   }
-
-  getChampions(){
-    return this.http.get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json');
+  getLeague() {
+    return this.http.get('http://localhost/lol/league/v4/entries/by-summoner/MdIvR5iCYxcgrQuim8YSNsLC2V59goVV5nBB7ZhG1lM?platform=LA2')
   }
-}
+  getMastery() {
+    return this.http.get('http://localhost/lol/champion-mastery/v4/champion-masteries/by-summoner/MdIvR5iCYxcgrQuim8YSNsLC2V59goVV5nBB7ZhG1lM?platform=LA2')
+  }
+  getMatchHistory() {
+    return this.http.get('http://localhost/lol/match/v4/matchlists/by-account/3crdujWAcvzS-kUGwOhgzieRwRteogq9OyijJQSwiA?platform=LA2')
+  }
+  getMatchInfo(matchid : String) {
+    return this.http.get('http://localhost/lol/match/v4/matches/'+matchid+'?platform=LA2')
+  }
+ }
