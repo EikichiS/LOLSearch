f00fec06e4d22977732415ff67c6dc1ccc72213b
Simon G. García <hc0867@bbvaplataforma.cl>
2019-10-08 17:55:21 -0300

    Enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SummonerService } from './services/summoner.service';
+import { ChampionsService } from './services/champions.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,17 @@ import { SummonerService } from './services/summoner.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'LOLSearch';
+  summoners: any = [];
+  leagues: any;
+  champions: any = [];
+  maestrias: any = [];
+  matchs: any = [];
+  matchsinfo: any = [];
+  gameid : '';
 
-  constructor(protected summonerservice: SummonerService) {
+  constructor(protected summonerservice: SummonerService,
+              private championservice: ChampionsService) {
   }
 
   ngOnInit() {
@@ -21,23 +31,52 @@ export class AppComponent {
         console.error(error);
       }),
       this.summonerservice.getLeague().subscribe(
-        (respuesta) => {
-          console.log(respuesta[0]);
-          this.leagues = respuesta[0];
+        (dataleague) => {
+          console.log(dataleague[0]);
+          this.leagues = dataleague[0];
           
         },
         (error) => {
           console.error(error);
         }),
-        this.summonerservice.getChampions().subscribe(
-          (response) => {
-            console.log(response);
-            this.champions = response;
+        this.championservice.getJSON().subscribe(
+          (datachampion) => {
+            console.log(datachampion);
+            this.champions = datachampion;
           },
           (error) => {
             console.error(error);
-          })
-        
+          }),
+          this.summonerservice.getMastery().subscribe(
+            (datamastery) => {
+              console.log(datamastery[0],datamastery[1],datamastery[2],datamastery[3],datamastery[4]);                                                  
+              this.maestrias = datamastery[0],datamastery[1],datamastery[2],datamastery[3],datamastery[4];
+            },
+            (error) => {
+              console.error(error);
+            }),
+            this.summonerservice.getMatchHistory().subscribe(
+              (datamatch) => {
+                console.log(datamatch['matches']);
+                console.log(datamatch['matches'][0].gameId);
+                this.matchs = datamatch['matches'];
+                this.gameid = datamatch['matches'].gameId;
+                for (var i = 0; i < 10; i++){
+                this.summonerservice.getMatchInfo(datamatch['matches'][i].gameId).subscribe(
+                  (datamatchinfo) => {
+                    console.log(datamatchinfo);
+                    this.matchsinfo = datamatchinfo;
+                  },
+                  (error) => {
+                    console.error(error);
+                  })
+                }
+              },
+              (error) => {
+                console.error(error);
+              }
+            )
+
   }
 
 
